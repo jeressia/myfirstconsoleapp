24916a30ec3c62041e80a6639192b18c1aca491a
Jeressia Williamson <jeressiajaytv@gmail.com>
2019-08-20 20:30:10 -0500

    testing

---
+++ myfirstconsoleapp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace myfirstconsoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myVariable = "Hello World";
+            Console.WriteLine(myVariable);
+        }
+    }
+}
